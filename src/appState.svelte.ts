6b8e7868edf2eccf70dd8422e525c421133e68ba
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-08-08 23:04:38 +0530

    Styling

---
+++ src/appState.svelte.ts
@@ -0,0 +1,6 @@
+
+
+
+export const appState = {
+    mobileSidebarOpened: false,
+}
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,131 @@
+<script>
+</script>
+
+<div
+	class="flex h-full grow flex-col gap-y-5 overflow-y-auto bg-gradient-to-b from-pink-500 to-purple-500 p-4 px-6"
+>
+	<nav class="flex flex-1 flex-col">
+		<ul role="list" class="flex flex-1 flex-col gap-y-7">
+			<li>
+				<ul role="list" class="-mx-2 space-y-1">
+					<li>
+						<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<i class="bi bi-journal-text text-xl"></i>
+							Test Series
+							<span
+								aria-hidden="true"
+								class="ml-auto w-9 min-w-max rounded-full bg-white/30 px-2.5 py-0.5 text-center text-xs/5 font-medium whitespace-nowrap text-white ring-1 ring-gray-200 backdrop-blur ring-inset"
+								>5</span
+							>
+						</a>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<i class="bi bi-globe text-xl"></i>
+							Current Affairs
+						</a>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<i class="bi bi-book text-xl"></i>
+							Editorials
+							<span
+								aria-hidden="true"
+								class="ml-auto w-9 min-w-max rounded-full bg-white/30 px-2.5 py-0.5 text-center text-xs/5 font-medium whitespace-nowrap text-white ring-1 ring-gray-200 backdrop-blur ring-inset"
+								>5</span
+							>
+						</a>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<i class="bi bi-mortarboard text-xl"></i>
+							Free Material
+							<span
+								aria-hidden="true"
+								class="ml-auto w-9 min-w-max rounded-full bg-white/30 px-2.5 py-0.5 text-center text-xs/5 font-medium whitespace-nowrap text-white ring-1 ring-gray-200 backdrop-blur ring-inset"
+								>5</span
+							>
+						</a>
+					</li>
+					<li>
+						<a
+							href="/quicklinks"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<i class="bi bi-link-45deg text-xl"></i>
+							Quick Links
+						</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<div class="text-xs/6 font-semibold text-gray-200">Exams</div>
+				<ul role="list" class="-mx-2 mt-2 space-y-1">
+					<li>
+						<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<span class="truncate">UPSC</span>
+						</a>
+					</li>
+					<li>
+						<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<span class="truncate">State PCS</span>
+						</a>
+					</li>
+					<li>
+						<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<span class="truncate">Railway</span>
+						</a>
+					</li>
+					<li>
+						<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
+						<a
+							href="#"
+							class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-white backdrop-blur hover:bg-white/30"
+						>
+							<span class="truncate">Police Bharti</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+			<!-- <li class="-mx-6 mt-auto">
+				<a
+					href="#"
+					class="flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white hover:bg-gray-800"
+				>
+					<img
+						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt=""
+						class="size-8 rounded-full bg-gray-800"
+					/>
+					<span class="sr-only">Your profile</span>
+					<span aria-hidden="true">Tom Cook</span>
+				</a>
+			</li> -->
+		</ul>
+	</nav>
+</div>
